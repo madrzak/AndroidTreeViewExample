05a3eaf70bf270b6ee3c458d4f84c3fc5c49216f
Lukasz Madrzak <l.madrzak@gmail.com>
2017-05-06 21:09:14 +0100

    seperate logic from view

--- app/src/main/java/com/madrzak/androidtreeviewmanytypes/MainActivity.java
+++ app/src/main/java/com/madrzak/androidtreeviewmanytypes/MainActivity.java
@@ -3,28 +3,14 @@ package com.madrzak.androidtreeviewmanytypes;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
 
-import com.madrzak.androidtreeviewmanytypes.model.Account;
-import com.madrzak.androidtreeviewmanytypes.model.Group;
-import com.madrzak.androidtreeviewmanytypes.model.MyNodeType;
-import com.madrzak.androidtreeviewmanytypes.model.MyTreeNodeViewModel;
-import com.madrzak.androidtreeviewmanytypes.treeview.IconTreeItemHolder;
-import com.madrzak.androidtreeviewmanytypes.treeview.SelectableHeaderHolder;
 import com.unnamed.b.atv.model.TreeNode;
 import com.unnamed.b.atv.view.AndroidTreeView;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
-
-import static rx.Observable.from;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -33,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
     RelativeLayout container;
 
     private AndroidTreeView tView;
-    private Map<Long, MyTreeNodeViewModel> map = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +30,10 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(view);
 
-        TreeNode root = generateTree(generateDummyData());
+        NodeTreeProvider.getInstance().init(this);
+
+        TreeNode root = NodeTreeProvider.getInstance().generateTree();
+
         tView = new AndroidTreeView(this, root);
         tView.setDefaultAnimation(true);
         tView.setSelectionModeEnabled(true);
@@ -59,63 +47,9 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        generateDummyData();
     }
 
 
-    private List<MyTreeNodeViewModel> generateDummyData() {
-        List<MyTreeNodeViewModel> result = new ArrayList<>();
-
-        result.add(new MyTreeNodeViewModel(new Account(1, "account 1"), null));
-        result.add(new MyTreeNodeViewModel(new Account(2, "account 2"), null));
-        result.add(new MyTreeNodeViewModel(new Account(3, "account 3"), 1L));
-        result.add(new MyTreeNodeViewModel(new Account(4, "account 4"), 1L));
-        result.add(new MyTreeNodeViewModel(new Account(5, "account 5"), 1L));
-        result.add(new MyTreeNodeViewModel(new Group(6, "group 6"), 2L));
-        result.add(new MyTreeNodeViewModel(new Group(7, "group 7"), 2L));
-
-        // assigning children id's to all nodes
-        from(result)
-                .doOnError(throwable -> Log.i(TAG, throwable.toString()))
-                .forEach(item -> {
-                    Log.i(TAG, "item " + item.getTreeNode().getName() + " " + item.getParentId() + " " + item.getTreeNode().getId());
-                    if (item.getParentId() != null) {
-                        map.get(item.getParentId()).addChild(item.getTreeNode().getId());
-                    }
-
-                    map.put(item.getTreeNode().getId(), item);
-                })
-        ;
-
-        return result;
-    }
-
-
-    private TreeNode generateTree(List<MyTreeNodeViewModel> data) {
-        TreeNode root = TreeNode.root();
-
-        from(data)
-                .filter(MyTreeNodeViewModel::isRoot)
-                .forEach(nodeViewModel -> {
-                    if (nodeViewModel.getTreeNode().getNodeType().equals(MyNodeType.ACCOUNT)) {
-                        TreeNode newNode = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_folder, nodeViewModel.getTreeNode().getName(), nodeViewModel.getTreeNode().getId())).setViewHolder(new SelectableHeaderHolder(this));
-                        root.addChild(newNode);
-                        buildBranch(newNode,nodeViewModel);
-
-                    }
-                });
-
-        return root;
-    }
-
-    private TreeNode buildBranch(TreeNode treeNode, MyTreeNodeViewModel model) {
-        TreeNode newNode = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_folder, model.getTreeNode().getName(), model.getTreeNode().getId())).setViewHolder(new SelectableHeaderHolder(this));
-        treeNode.addChild(newNode);
-        for (Long nodeId : model.getChildren()) {
-            buildBranch(newNode, map.get(nodeId));
-        }
-        return newNode;
-    }
 
 
 }
--- app/src/main/java/com/madrzak/androidtreeviewmanytypes/NodeTreeProvider.java
+++ app/src/main/java/com/madrzak/androidtreeviewmanytypes/NodeTreeProvider.java
@@ -1,11 +1,94 @@
 package com.madrzak.androidtreeviewmanytypes;
 
+import android.content.Context;
+import android.util.Log;
+
+import com.madrzak.androidtreeviewmanytypes.model.Account;
+import com.madrzak.androidtreeviewmanytypes.model.Group;
+import com.madrzak.androidtreeviewmanytypes.model.MyNodeType;
+import com.madrzak.androidtreeviewmanytypes.model.MyTreeNodeViewModel;
+import com.madrzak.androidtreeviewmanytypes.model.User;
+import com.madrzak.androidtreeviewmanytypes.treeview.IconTreeItemHolder;
+import com.madrzak.androidtreeviewmanytypes.treeview.SelectableHeaderHolder;
+import com.unnamed.b.atv.model.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static rx.Observable.from;
+
 /**
  * Created by Łukasz on 05/05/2017.
  */
 
 public class NodeTreeProvider {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
+    private Context context;
+
+    private static final NodeTreeProvider instance = new NodeTreeProvider();
+
+    static NodeTreeProvider getInstance() {
+        return instance;
+    }
+
+    void init(Context context) {
+        this.context = context;
+        generateDummyData();
+    }
+
+    private Map<Long, MyTreeNodeViewModel> nodeViews = new HashMap<>();
+
+
+    public TreeNode generateTree() {
+        TreeNode root = TreeNode.root();
+
+        from(nodeViews.values())
+                .filter(MyTreeNodeViewModel::isRoot)
+                .forEach(nodeViewModel -> {
+                    if (nodeViewModel.getTreeNode().getNodeType().equals(MyNodeType.ACCOUNT)) {
+                        buildBranch(root, nodeViewModel);
+                    }
+                });
+
+        return root;
+    }
+
+
+    private void generateDummyData() {
+
+        nodeViews.put(1L, (new MyTreeNodeViewModel(new Account(1, "account 1"), null)));
+        nodeViews.put(2L, (new MyTreeNodeViewModel(new Account(2, "account 2"), null)));
+        nodeViews.put(3L, (new MyTreeNodeViewModel(new Account(3, "account 3"), 1L)));
+        nodeViews.put(4L, (new MyTreeNodeViewModel(new Account(4, "account 4"), 1L)));
+        nodeViews.put(5L, (new MyTreeNodeViewModel(new Account(5, "account 5"), 1L)));
+        nodeViews.put(6L, (new MyTreeNodeViewModel(new Group(6, "group 6"), 2L)));
+        nodeViews.put(7L, (new MyTreeNodeViewModel(new Group(7, "group 7"), 2L)));
+        nodeViews.put(8L, (new MyTreeNodeViewModel(new User(8, "user 1"), 2L)));
+        nodeViews.put(9L, (new MyTreeNodeViewModel(new Account(9, "account 3"), null)));
+
+        // assigning children id's to all nodes
+        from(nodeViews.values())
+                .doOnError(throwable -> Log.i(TAG, throwable.toString()))
+                .forEach(item -> {
+                    Log.i(TAG, "item " + item.getTreeNode().getName() + " " + item.getParentId() + " " + item.getTreeNode().getId());
+                    if (item.getParentId() != null) {
+                        nodeViews.get(item.getParentId()).addChild(item.getTreeNode().getId());
+                    }
+
+                    nodeViews.put(item.getTreeNode().getId(), item);
+                });
+    }
+
+
+    private TreeNode buildBranch(TreeNode treeNode, MyTreeNodeViewModel model) {
+        TreeNode newNode = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_folder, model.getTreeNode().getName(), model.getTreeNode().getId())).setViewHolder(new SelectableHeaderHolder(context));
+        treeNode.addChild(newNode);
+        for (Long nodeId : model.getChildren()) {
+            buildBranch(newNode, nodeViews.get(nodeId));
+        }
+        return newNode;
+    }
 
 
 }
